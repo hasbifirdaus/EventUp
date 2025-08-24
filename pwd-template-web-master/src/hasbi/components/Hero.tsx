b0ff43d1f8bfe4c66a39dda9fe1d843158ad73eb
hasbi firdaus fauzun <hasbi.fauzun@gmail.com>
2025-08-24 22:13:46 +0700

    chore: setup tailwindcss configuration

---
+++ pwd-template-web-master/src/hasbi/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section id="section-hero" className="bg-gray-600">
+      <div id="swap-banner"></div>
+      <div id="featured-events"></div>
+      <div id="banner"></div>
+    </section>
+  );
+};
+
+export default Hero;
