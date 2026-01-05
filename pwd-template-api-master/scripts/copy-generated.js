1e8ed0f6b649f5ceb7772126b74d34f9830d1bf9
hasbi firdaus fauzun <hasbi.fauzun@gmail.com>
2026-01-06 01:42:47 +0700

    creat scripts folder and add build: tsc && npx prisma generate && node scripts/copy-generated.js in package.json

---
+++ pwd-template-api-master/scripts/copy-generated.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+function copyFolder(src, dest) {
+  if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true });
+  const entries = fs.readdirSync(src, { withFileTypes: true });
+
+  for (let entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+
+    if (entry.isDirectory()) {
+      copyFolder(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
+
+copyFolder("src/generated", "dist/generated");
+console.log("✅ Prisma generated copied to dist/generated");
